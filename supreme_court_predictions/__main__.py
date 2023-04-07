45c0ca821a0a3f0801a7262dd8b25bda368b8413
michplunkett <michplunkett@users.noreply.github.com>
2023-04-07 13:47:49 -0500

    Update __main__.py

--- supreme_court_predictions/__main__.py
+++ supreme_court_predictions/__main__.py
@@ -1,7 +1,26 @@
 """
-This file serves as the entry point for the project module.
+This file serves as the entry point for the supreme_court_predictions module.
 """
+
+import argparse
+
+from supreme_court_predictions.api.convokit.client import get_data
 
 # pylint: disable=invalid-name
 if __name__ == "__main__":
     print("oh, what up?")
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--get-data",
+        help="Get data from Convokit",
+        type=bool,
+        default=False,
+        action=argparse.BooleanOptionalAction,
+    )
+
+    args = parser.parse_args()
+
+    if args.get_data:
+        get_data()
