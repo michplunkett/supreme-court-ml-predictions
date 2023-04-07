1783f13be802ac391aeb5522cd5dce4361057af4
michplunkett <michplunkett@users.noreply.github.com>
2023-04-07 13:50:17 -0500

    Update client.py

--- supreme_court_predictions/api/convokit/client.py
+++ supreme_court_predictions/api/convokit/client.py
@@ -1,5 +1,8 @@
-# Download the conversation kit, unzip it, and store it in the /data/folder
+"""
+This file serves as the client for convokit.
+"""
 
 
 def get_data():
+    # Download the conversation kit, unzip it, and store it in the /data/folder
     print("getting that data")
