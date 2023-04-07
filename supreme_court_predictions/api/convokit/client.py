cba75ec937aeffd9bd9e1d33069a9946f738c817
michplunkett <michplunkett@users.noreply.github.com>
2023-04-07 14:31:07 -0500

    Update client.py

--- supreme_court_predictions/api/convokit/client.py
+++ supreme_court_predictions/api/convokit/client.py
@@ -11,5 +11,6 @@ def get_data():
 
     print("Loading Supreme Court Corpus Data...")
     corpus = Corpus(filename=download("supreme-corpus"))
-    corpus.dump("supreme_corpus",
-                base_path="./supreme_court_predictions/data/convokit/")
+    corpus.dump(
+        "supreme_corpus", base_path="./supreme_court_predictions/data/convokit/"
+    )
