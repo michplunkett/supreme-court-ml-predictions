81011f741c83bcce8262f9e5c2df40981fad01d5
Chanteria Milner <chanteria.milner@gmail.com>
2023-04-07 14:29:11 -0500

    Adding data download function

--- supreme_court_predictions/api/convokit/client.py
+++ supreme_court_predictions/api/convokit/client.py
@@ -1,8 +1,15 @@
 """
 This file serves as the client for convokit.
 """
+from convokit import Corpus, download
 
 
 def get_data():
-    # Download the conversation kit, unzip it, and store it in the /data/folder
-    print("getting that data")
+    """
+    Loads and outputs the Supreme Court Corpus data
+    """
+
+    print("Loading Supreme Court Corpus Data...")
+    corpus = Corpus(filename=download("supreme-corpus"))
+    corpus.dump("supreme_corpus",
+                base_path="./supreme_court_predictions/data/convokit/")
