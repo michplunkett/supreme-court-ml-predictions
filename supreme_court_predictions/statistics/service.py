755b7782f1e65c4d763db9878a76a7e07c5a98f2
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-04-12 19:51:46 -0500

    Update service.py

--- supreme_court_predictions/statistics/service.py
+++ supreme_court_predictions/statistics/service.py
@@ -6,4 +6,4 @@ from supreme_court_predictions.statistics.datacleaner import DataCleaner
 
 
 def main():
-    DataCleaner(downloaded_corpus=True, save_data=True)
+    DataCleaner(downloaded_corpus=True, save_data=True).parse_all_data()
