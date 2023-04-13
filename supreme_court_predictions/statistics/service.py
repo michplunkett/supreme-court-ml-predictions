06d4273aaf71350dc455b99a60a1aca4bc8647fd
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-04-12 19:45:10 -0500

    Update service.py

--- supreme_court_predictions/statistics/service.py
+++ supreme_court_predictions/statistics/service.py
@@ -2,7 +2,7 @@
 This file works as the central point of interaction for the statistics package.
 """
 
-from supreme_court_predictions.statistics.DataCleaner import DataCleaner
+from supreme_court_predictions.statistics.datacleaner import DataCleaner
 
 
 def main():
