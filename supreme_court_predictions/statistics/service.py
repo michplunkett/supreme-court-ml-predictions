ee7e1e88650c7acace6f2220e5d37b4081f69ad5
Chanteria Milner <chanteria.milner@gmail.com>
2023-04-18 15:16:42 -0500

    Updating make file

--- supreme_court_predictions/statistics/service.py
+++ supreme_court_predictions/statistics/service.py
@@ -6,6 +6,9 @@ from supreme_court_predictions.statistics.datacleaner import DataCleaner
 from supreme_court_predictions.statistics.descriptives import Descriptives
 
 
-def main():
+def clean_data():
     DataCleaner(downloaded_corpus=True, save_data=True).parse_all_data()
+
+
+def describe_data():
     Descriptives(downloaded_clean_corpus=True, save_data=True).parse_all_data()
