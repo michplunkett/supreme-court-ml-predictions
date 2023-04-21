84edb4554b588b2d4fdaf4b4f9a51a240abd5bfc
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-04-21 16:21:37 -0500

    Update client.py

--- supreme_court_predictions/api/convokit/client.py
+++ supreme_court_predictions/api/convokit/client.py
@@ -1,16 +1,29 @@
 """
 This file serves as the client for convokit.
 """
+import requests
 from convokit import Corpus, download
+
+from supreme_court_predictions.util.contants import ENCODING_UTF_8
+
+# Constants
+DOWNLOAD_BASE_PATH = "./supreme_court_predictions/data/convokit/"
 
 
 def get_data():
     """
-    Loads and outputs the Supreme Court Corpus data
+    Loads and outputs the Supreme Court Corpus and case verdict data
     """
 
     print("Loading Supreme Court Corpus Data...")
     corpus = Corpus(filename=download("supreme-corpus"))
-    corpus.dump(
-        "supreme_corpus", base_path="./supreme_court_predictions/data/convokit/"
+    corpus.dump("supreme_corpus", base_path=DOWNLOAD_BASE_PATH)
+
+    r = requests.get(
+        url="https://zissou.infosci.cornell.edu/convokit"
+        "/datasets/supreme-corpus/cases.jsonl",
+        allow_redirects=True,
     )
+
+    with open(f"{DOWNLOAD_BASE_PATH}cases.jsonl", "w") as outfile:
+        outfile.write(r.content.decode(ENCODING_UTF_8))
