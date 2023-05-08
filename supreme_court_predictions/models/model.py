0bac67811912b5edad03732e1a72b8960e9f8d64
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-05-08 12:54:35 -0500

    Update model.py

--- supreme_court_predictions/models/model.py
+++ supreme_court_predictions/models/model.py
@@ -16,6 +16,15 @@ class Model(ABC):
         Creates the model and returns an accuracy score.
 
         :param df: A dataframe used to create the model.
+        :return: A sklearn model.
+        """
+
+    @abstractmethod
+    def create_and_measure(self, df):
+        """
+        Takes in a dataframe and returns the applicable accuracy measurement.
+
+        :param df: A dataframe used to create the model.
         :return: Float of some accuracy measurement.
         """
 
