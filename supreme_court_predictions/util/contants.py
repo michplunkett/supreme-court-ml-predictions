18611b6dedc79c61956827b9610875f8c86c0068
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-05-08 22:37:07 -0500

    Update contants.py

--- supreme_court_predictions/util/contants.py
+++ supreme_court_predictions/util/contants.py
@@ -4,6 +4,9 @@ This file holds constants that will be used throughout the application.
 
 ENCODING_UTF_8 = "utf-8"
 
+# sklearn things
+SEED_CONSTANT = 8675309
+
 # File modes
 FILE_MODE_READ = "r"
 FILE_MODE_WRITE = "w"
