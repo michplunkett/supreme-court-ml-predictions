48db037c8f73f8e0f9f3c29f0b577a4672c2a179
Chanteria Milner <chanteria.milner@gmail.com>
2023-05-12 23:22:16 -0500

    Reoptimize models (#38)
    <!--- Please write your PR name in the present imperative tense.
    Examples of that tense are: "Fix issue in the dispatcher where…",
    "Improve our handling of…", etc." -->
    <!-- For more information on Pull Requests, you can reference here:
    https://success.vanillaforums.com/kb/articles/228-using-pull-requests-to-contribute
    -->
    ## Describe your changes
    Altered the way `self.print_results()` was called to account for
    potential modeling errors.
    
    ## Checklist before requesting a review
    <!--- These are suggested things you could add, but what you add will be
    dependent on your repository's standards. --->
    - [x] The code runs successfully.
    
    ```
    (supreme-court-predictions-py3.11) shaymilner@MacBook-Air-4 supreme-court-ml-predictions % make format
    isort "supreme_court_predictions"/ test/
    Skipped 1 files
    black "supreme_court_predictions"/ test/
    All done! ✨ 🍰 ✨
    23 files left unchanged.
    (supreme-court-predictions-py3.11) shaymilner@MacBook-Air-4 supreme-court-ml-predictions % make lint
    ruff "supreme_court_predictions"/ test/
    (supreme-court-predictions-py3.11) shaymilner@MacBook-Air-4 supreme-court-ml-predictions % make test
    pytest -vs test/
    ================== test session starts ===================
    platform darwin -- Python 3.11.1, pytest-7.3.1, pluggy-1.0.0 -- /Users/shaymilner/Library/Caches/pypoetry/virtualenvs/supreme-court-predictions-sGlGetDh-py3.11/bin/python
    cachedir: .pytest_cache
    rootdir: /Users/shaymilner/Documents/Harris/Spring23/Machine_Learning/final_project/supreme-court-ml-predictions
    plugins: anyio-3.6.2
    collected 1 item                                         
    
    test/test_util.py::test_get_full_pathway PASSED
    
    =================== 1 passed in 0.01s ==================== 
    ```
    - [x] I ran `make lint` and have made the bulk of changes that it has
    requested.

--- supreme_court_predictions/models/random_forest.py
+++ supreme_court_predictions/models/random_forest.py
@@ -6,11 +6,12 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
+from supreme_court_predictions.models.model import Model
 from supreme_court_predictions.util.contants import SEED_CONSTANT
 from supreme_court_predictions.util.files import get_full_data_pathway
 
 
-class RandomForest:
+class RandomForest(Model):
     """
     This class runs sklearn random forest on aggregated utterance
     from the Supreme Court dataset.
@@ -44,17 +45,17 @@ class RandomForest:
         self.dataframe_names = []
 
         for df in dfs:
-            # make sure its a file name
+            # Ensure the file exists
             if os.path.isfile(self.local_path + df):
-                # pickle file
+                # Pickle file
                 if (df.split(".")[-1]) == "p":
                     self.dataframes.append(pd.read_pickle(self.local_path + df))
 
-                # csv file
+                # CSV File
                 else:
                     self.dataframes.append(pd.read_csv(self.local_path + df))
 
-                # add name of file
+                # Add name of file
                 self.dataframe_names.append(df.split(".")[0])
 
     def create(self, df):
@@ -88,7 +89,7 @@ class RandomForest:
         if self.print:
             print("Starting the Random Forest")
         forest = RandomForestClassifier(
-            num_trees=self.num_trees, max_depth=self.max_depth
+            n_estimators=self.num_trees, max_depth=self.max_depth
         )
 
         # Fit the classifier on the training data
@@ -102,24 +103,17 @@ class RandomForest:
         Runs the create function on each type of aggregated utterance.
         """
 
-        for df in self.dataframes:
+        for df, dfname in zip(self.dataframes, self.dataframe_names):
             try:
-                # accuracy score method to be used later from ABC
-                _, y_test, y_pred = self.create(df)
-                acc = accuracy_score(y_true=y_test, y_pred=y_pred)
+                acc = self.create_and_measure(df, accuracy_score)
                 self.accuracies.append(acc)
+
+                # Print the results, if applicable
+                if self.print:
+                    self.print_results(self.name.lower(), acc, dfname)
             except ValueError:
                 print("------------------------------------------")
                 print("Error: training data is not big enough for this subset")
-                print("------------------------------------------")
-
-        # Print the results, if applicable
-        if self.print:
-            # This will also be later used from ABC
-            for acc, df_name in zip(self.accuracies, self.dataframe_names):
-                print("------------------------------------------")
-                print(f"Running a {self.name.lower()} on {df_name}...")
-                print(f"Accuracy score: {acc}")
                 print("------------------------------------------")
 
         return self.accuracies
@@ -131,11 +125,17 @@ class RandomForest:
         :return string representation of Model
         """
 
-        parameters = [self.max_features, self.test_size, self.max_iter]
+        parameters = [
+            self.max_features,
+            self.test_size,
+            self.num_trees,
+            self.max_depth,
+        ]
         parameter_names = [
-            "Maximum Features:",
-            "Test Size:",
-            "Maximum Iterations:",
+            "Maximum Features",
+            "Test Size",
+            "Number of Trees",
+            "Maximum Depth",
         ]
 
         return_str = f"MODEL TYPE: {self.name}\n"
