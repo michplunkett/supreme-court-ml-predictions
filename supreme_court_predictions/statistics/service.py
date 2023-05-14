fda0751131e32e4a70420b0c65a3a0af6c77c228
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-05-14 18:47:38 -0500

    Update service.py

--- supreme_court_predictions/statistics/service.py
+++ supreme_court_predictions/statistics/service.py
@@ -6,9 +6,21 @@ from supreme_court_predictions.processing.datacleaner import DataCleaner
 from supreme_court_predictions.statistics.descriptives import Descriptives
 
 
-def clean_data():
-    DataCleaner().parse_all_data()
+def clean_data(debug_mode=False):
+    """
+    Runner function for cleaning and formatting the Convokit corpus.
+
+    :param bool debug_mode: Indicates if the application requires debug print
+        statements.
+    """
+    DataCleaner(debug_mode).parse_all_data()
 
 
-def describe_data():
-    Descriptives().parse_all_data()
+def describe_data(debug_mode=False):
+    """
+    Runner function for calculating the descriptive statistics.
+
+    :param bool debug_mode: Indicates if the application requires debug print
+        statements.
+    """
+    Descriptives(debug_mode).parse_all_data()
