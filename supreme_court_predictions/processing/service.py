effc1ec81dc3da4580d5ec3e1d86b17ac20ec1a9
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-05-14 18:47:59 -0500

    Update service.py

--- supreme_court_predictions/processing/service.py
+++ supreme_court_predictions/processing/service.py
@@ -8,9 +8,21 @@ from supreme_court_predictions.processing.token_aggregation import (
 from supreme_court_predictions.processing.tokenizer import Tokenizer
 
 
-def tokenize_data():
-    Tokenizer()
+def tokenize_data(debug_mode=False):
+    """
+    Runner function for tokenizing data.
+
+    :param bool debug_mode: Indicates if the application requires debug print
+        statements.
+    """
+    Tokenizer(debug_mode)
 
 
-def process_data():
-    TokenAggregations().parse_all_data()
+def process_data(debug_mode=False):
+    """
+    Runner function for aggregating tokens.
+
+    :param bool debug_mode: Indicates if the application requires debug print
+        statements.
+    """
+    TokenAggregations(debug_mode).parse_all_data()
