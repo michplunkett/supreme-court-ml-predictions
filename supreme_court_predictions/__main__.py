70334034438a04a5753303c43835321be4478bd4
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-05-14 18:49:07 -0500

    Update __main__.py

--- supreme_court_predictions/__main__.py
+++ supreme_court_predictions/__main__.py
@@ -90,26 +90,42 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
+    # Debug mode will only trigger when we call functionality from the command
+    # line, which means we shouldn't get an obscene amount of printouts
+    # in our Jupyter notebooks.
+    debug_mode = any(
+        [
+            args.get_data,
+            args.clean_data,
+            args.tokenize_data,
+            args.describe_data,
+            args.process_data,
+            args.logistic_regression,
+            args.random_forest,
+            args.xg_boost,
+        ]
+    )
+
     if args.get_data:
-        get_data()
+        get_data(debug_mode)
 
     if args.clean_data:
-        clean_data()
+        clean_data(debug_mode)
 
     if args.tokenize_data:
-        tokenize_data()
+        tokenize_data(debug_mode)
 
     if args.describe_data:
-        describe_data()
+        describe_data(debug_mode)
 
     if args.process_data:
-        process_data()
+        process_data(debug_mode)
 
     if args.logistic_regression:
-        run_linear_regression()
+        run_linear_regression(debug_mode)
 
     if args.random_forest:
-        run_random_forest()
+        run_random_forest(debug_mode)
 
     if args.xg_boost:
-        run_xg_boost()
+        run_xg_boost(debug_mode)
