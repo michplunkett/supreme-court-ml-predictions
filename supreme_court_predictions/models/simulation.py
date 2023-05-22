7d5629beacf9042df48f8c9ac6251e7cdf7c2e79
JJModern <example@example.com>
2023-05-22 12:19:11 -0500

    simulation quasi operational

--- supreme_court_predictions/models/simulation.py
+++ supreme_court_predictions/models/simulation.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+from collections import Counter
 
 from supreme_court_predictions.util.constants import SEED_CONSTANT
 from supreme_court_predictions.util.functions import get_full_data_pathway
@@ -45,11 +46,13 @@ bag_of_words = vectorizer.fit_transform(merged_df['tokens'])
 models = {}
 accuracies = {}
 f1_scores = {}
+predictions = {}
 
 # Iterate over each unique speaker
 for speaker in merged_df['speaker'].unique():
     # Subset the data for the current speaker
     speaker_df = merged_df[merged_df['speaker'] == speaker]
+    predictions[speaker] = {}
 
     # If there's only one instance for a class, predict it as the single class
     if len(speaker_df['vote'].unique()) == 1:
@@ -58,43 +61,74 @@ for speaker in merged_df['speaker'].unique():
         f1_scores[speaker] = 1.0
     else:
         try:
+            token_df = pd.DataFrame(bag_of_words.toarray(), columns=vectorizer.get_feature_names_out())
+            token_df['case_id'] = merged_df['case_id'].values
             # Split the data into training and test sets
             X_train, X_test, y_train, y_test = train_test_split(
-                bag_of_words[speaker_df.index],
-                speaker_df['vote'],
+                token_df,
+                merged_df['vote'],
                 test_size=0.2,
                 random_state=42,
-                stratify=speaker_df['vote']
+                stratify=merged_df['vote']
             )
 
-            # Fit the logistic regression model if there are more than one instance of each class
-            lr = LogisticRegression(max_iter=1000, random_state=42)
-            lr.fit(X_train, y_train)
-
-            # Make predictions for the test set
-            y_pred = lr.predict(X_test)
-
-            # Calculate the accuracy and F1-score
-            acc = accuracy_score(y_test, y_pred)
-            f1 = f1_score(y_test, y_pred, average='macro')
-
-            # Store the model and scores in the dictionaries
-            models[speaker] = lr
-            accuracies[speaker] = acc
-            f1_scores[speaker] = f1
-
-            # Predict the single instance of the class
-            if len(y_test) == 0:
-                y_pred_single = lr.predict(X_test[:1])
-                # check if the judge X_test
-            else:
-                y_pred_single = default_class if y_pred[0] != y_pred[1] else y_pred[0]
-
-            # Print the prediction
-            print("Prediction for single instance:", y_pred_single)
         except ValueError:
-            print("Dataset too small")
+            print("Dataset too small for splitting")
+            continue
+        
+        if (len(y_test) != 0):
+            try:    
+                # Fit the logistic regression model if there are more than one instance of each class
+                lr = LogisticRegression(max_iter=1000, random_state=42)
+                lr.fit(X_train, y_train)
 
-print('Models:', models)
-print('Accuracies:', accuracies)
-print('F1 scores:', f1_scores)
+                # Make predictions for the test set
+                case_ids = X_test['case_id'].values
+
+                X_train = X_train.drop(columns='case_id')
+                X_test = X_test.drop(columns='case_id')
+
+                y_pred = lr.predict(X_test)
+
+                for case_id, pred_speaker_tuple in zip(case_ids, [(pred, speaker) for pred in y_pred]):
+                    predictions[case_id] = predictions.get(case_id, []) + [pred_speaker_tuple]
+
+                # Calculate the accuracy and F1-score
+                acc = accuracy_score(y_test, y_pred)
+                f1 = f1_score(y_test, y_pred, average='macro')
+
+                # Store the model and scores in the dictionaries
+                models[speaker] = lr
+                accuracies[speaker] = acc
+                f1_scores[speaker] = f1
+
+                # Print the prediction
+                print("Prediction for single instance:", y_pred_single)
+            except ValueError:
+                print("Prediction Error")
+
+print('Models by judges:', models)
+print('Accuracies by judges:', accuracies)
+print('F1 scores by judges:', f1_scores)
+
+majority_predictions, actual_values_dict = {}, {}
+
+# create dictionary for predictions
+for case_id, pred_speaker_tuples in predictions.items():
+    only_preds = [tup[0] for tup in pred_speaker_tuples]  # Extract all predictions for this case_id
+    counter = Counter(only_preds)
+
+    if len(counter.most_common(1)) != 0:
+        majority_predictions[case_id] = counter.most_common(1)[0][0]
+
+# create dictionary for actual
+for case_id, actual_value in zip(case_ids, y_test):
+    actual_values_dict[case_id] = actual_values_dict.get(case_id, []) + [actual_value]
+
+actual_values = [actual_values_dict[case_id] for case_id in majority_predictions.keys()]
+predicted_values = list(majority_predictions.values())
+
+# Calculate the accuracy
+total_accuracy = accuracy_score(actual_values, predicted_values)
+
+print('Total Accuracy for All Cases:', total_accuracy)
